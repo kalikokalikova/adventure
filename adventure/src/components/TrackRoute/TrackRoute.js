035d519f00a159a526ab46ce6084e30068e44d06
Alison Lee <alison2686@gmail.com>
2022-11-18 14:59:07 -0800

    add helper functions to TrackRoute

--- adventure/src/components/TrackRoute/TrackRoute.js
+++ adventure/src/components/TrackRoute/TrackRoute.js
@@ -1,23 +1,47 @@
 import React from 'react'
 import DeckGL from '@deck.gl/react';
 import {LineLayer} from '@deck.gl/layers';
+import {StaticMap} from 'react-map-gl'
+import data from '../../Utils/trackroutedata.json'
 
+const INITIAL_VIEW_STATE = {
+    latitude: 45.4211,
+    longitude: -75.6903,
+    zoom: 9,
+    maxZoom: 16,
+    pitch: 50,
+    bearing: 0
+  };
 
-const TrackRoute = ({data, viewState}) => {
+  const MAP_STYLE = "mapbox://styles/mapbox/streets-v9"
 
-    data = [{ 
-        inbound: 72633,
-        outbound: 74735,
-        from: {
-          name: 'Fremont Troll',
-          coordinates: [-75.3372987731628, 45.383321536272049]
-        },
-        to: {
-          name: 'Dahlia garden',
-          coordinates: [-75.546518086577947, 45.467134581917357]
-      },
+  function getColor(d) {
+    const z = d.start[2];
+    const r = z / 10000;
+  
+    return [255 * (1 - r * 2), 128 * r, 255 * r, 255 * (1 - r)];
+  }
+  
+  function getSize(type) {
+    if (type.search('major') >= 0) {
+      return 100;
+    }
+    if (type.search('small') >= 0) {
+      return 30;
+    }
+    return 60;
+  }
+  
+  function getTooltip({object}) {
+    return (
+      object &&
+      `\
+    ${object.country || object.abbrev || ''}
+    ${object.name.indexOf('0x') >= 0 ? '' : object.name}`
+    );
+  }
 
-    }]
+const TrackRoute = () => {
 
       const layer = new LineLayer({
         id: 'line-layer',
@@ -34,8 +58,13 @@ const TrackRoute = ({data, viewState}) => {
         <DeckGL 
             viewState={viewState}
             layers={[layer]}
-            getTooltip={({object}) => object && `${object.from.name} to ${object.to.name}`} 
-        />
+            getTooltip={({object}) => object && `${object.from.name} to ${object.to.name}`}
+        >
+            <StaticMap>
+
+            </StaticMap>
+        </DeckGL> 
+
     </div>
   )
 }

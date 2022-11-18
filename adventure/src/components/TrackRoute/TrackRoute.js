2616e971915d9394b1dcf1ed16db4dc32838dc1a
Alison Lee <alison2686@gmail.com>
2022-11-18 14:47:46 -0800

    add TrackRoute component

--- adventure/src/components/TrackRoute/TrackRoute.js
+++ adventure/src/components/TrackRoute/TrackRoute.js
@@ -1,9 +1,42 @@
 import React from 'react'
-import DeckGL from '@decl.gl/react'
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
 
-const TrackRoute = () => {
+
+const TrackRoute = ({data, viewState}) => {
+
+    data = [{ 
+        inbound: 72633,
+        outbound: 74735,
+        from: {
+          name: 'Fremont Troll',
+          coordinates: [-75.3372987731628, 45.383321536272049]
+        },
+        to: {
+          name: 'Dahlia garden',
+          coordinates: [-75.546518086577947, 45.467134581917357]
+      },
+
+    }]
+
+      const layer = new LineLayer({
+        id: 'line-layer',
+        data,
+        pickable: true,
+        getWidth: 50,
+        getSourcePosition: d => d.from.coordinates,
+        getTargetPosition: d => d.to.coordinates,
+        getColor: d => [Math.sqrt(d.inbound + d.outbound), 140, 0]
+      });
+
   return (
-    <div>TrackRoute</div>
+    <div>
+        <DeckGL 
+            viewState={viewState}
+            layers={[layer]}
+            getTooltip={({object}) => object && `${object.from.name} to ${object.to.name}`} 
+        />
+    </div>
   )
 }
 
